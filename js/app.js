d20ab1ffec5e6f6deba6e790d688befa84fc2f5b
Julien <julien.linder@gmail.com>
2017-02-12 21:29:21 +0100

    dernier push

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
 	alert("Bienvenue à toi utilisateur de la todolist! ;-)");
 	console.log("Merci de bien gérer les tâches");
-	$(".liste").append("<li>" + "ANALYSER LE CODE AVANT DE COMMENCER" + "</li>");
+	
+	$(".cocher").click(function(){
+		var ca=$(this).prev();
+		$(this).toggleClass("valide");
+		ca.toggleClass("valide");
+		console.log("magique");
+	})
 
 	$("#ajouter").click(function(){
 		var ajout=$("#ajoutinp").val();
-		$(".liste").append("<li>" + ajout + "</li>");
+		$(".liste").append("<li class='element nonvalide'>" + ajout + "</li>");
+		$(".liste").append($("<button>X</button>").click(function(){
+			var ca=$(this).prev();
+			$(this).toggleClass("valide");
+			ca.toggleClass("valide");
+		}))
+		$("#ajoutinp").val("");
 		localStorage.setItem("ajoutinp", ajout);
 	})
 
+	$('#done').click(function(){
+		$("")
+	})
+	$('#todo').click(function(){
+		$(".valide").css("display", "none");
+	})
+	$('#all').click(function(){
 
+	})
 
 
 
